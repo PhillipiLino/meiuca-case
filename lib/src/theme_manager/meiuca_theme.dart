f5bb45de93b3fac08f4412e82f058a6fddb314b0
PhillipiLino <phillipi.lino@gmail.com>
2023-12-05 10:10:13 -0300

    Create spacing squish sizes property

--- lib/src/theme_manager/meiuca_theme.dart
+++ lib/src/theme_manager/meiuca_theme.dart
@@ -1,11 +1,13 @@
 import 'properties/meiuca_theme_border_size.dart';
 import 'properties/meiuca_theme_opacity_level.dart';
 import 'properties/meiuca_theme_spacing_size.dart';
+import 'properties/meiuca_theme_spacing_squish_size.dart';
 
 class MeiucaTheme {
   final MeiucaThemeBorderSize borderSizes;
   final MeiucaThemeOpacityLevel opacityLevels;
   final MeiucaThemeSpacingSize spacingSizes;
+  final MeiucaThemeSpacingSquishSize spacingSquishSizes;
 
   static const String _border = 'border';
   static const String _size = 'size';
@@ -14,22 +16,27 @@ class MeiucaTheme {
   static const String _level = 'level';
 
   static const String _spacing = 'spacing';
+  static const String _spacingSquish = 'spacing_squish';
 
   MeiucaTheme({
     required this.borderSizes,
     required this.opacityLevels,
     required this.spacingSizes,
+    required this.spacingSquishSizes,
   });
 
   MeiucaTheme.fromJSON(Map<String, dynamic> json)
       : borderSizes = MeiucaThemeBorderSize.fromJSON(json[_border][_size]),
         opacityLevels =
             MeiucaThemeOpacityLevel.fromJSON(json[_opacity][_level]),
-        spacingSizes = MeiucaThemeSpacingSize.fromJSON(json[_spacing][_size]);
+        spacingSizes = MeiucaThemeSpacingSize.fromJSON(json[_spacing][_size]),
+        spacingSquishSizes =
+            MeiucaThemeSpacingSquishSize.fromJSON(json[_spacingSquish][_size]);
 
   toJSON() => {
         _border: {_size: borderSizes.toJSON()},
         _opacity: {_level: opacityLevels.toJSON()},
         _spacing: {_size: spacingSizes.toJSON()},
+        _spacingSquish: {_size: spacingSquishSizes.toJSON()},
       };
 }
---
+++ lib/src/theme_manager/properties/meiuca_theme_spacing_squish_size.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../meiuca_theme_property_data.dart';
+import '../meiuca_theme_property_data_attributes.dart';
+
+class MeiucaThemeSpacingSquishSize {
+  final EdgeInsets quarck;
+  final EdgeInsets nano;
+  final EdgeInsets xs;
+  final EdgeInsets sm;
+
+  static const String _stack = 'stack';
+  static const String _vertical = 'v';
+  static const String _horizontal = 'h';
+
+  static const String _quarck = 'quarck';
+  static const String _nano = 'nano';
+  static const String _xs = 'xs';
+  static const String _sm = 'sm';
+
+  MeiucaThemeSpacingSquishSize({
+    required this.quarck,
+    required this.nano,
+    required this.xs,
+    required this.sm,
+  });
+
+  MeiucaThemeSpacingSquishSize.fromJSON(Map<String, dynamic> json)
+      : quarck = _getValueInEdgeInsets(json[_quarck]),
+        nano = _getValueInEdgeInsets(json[_nano]),
+        xs = _getValueInEdgeInsets(json[_xs]),
+        sm = _getValueInEdgeInsets(json[_sm]);
+
+  Map<String, dynamic> toJSON() => {
+        _quarck: _createPropertyData(quarck),
+        _nano: _createPropertyData(nano),
+        _xs: _createPropertyData(xs),
+        _sm: _createPropertyData(sm),
+      };
+
+  static EdgeInsets _getValueInEdgeInsets(Map<String, dynamic> json) {
+    final property = MeiucaThemePropertyData.fromJSON(json[_stack]);
+    final value = property.value.replaceAll('px', '');
+    final valuesList = value.split(' ');
+    final vertical = double.tryParse(valuesList.firstOrNull ?? '0') ?? 0;
+    final horizontal = double.tryParse(valuesList.lastOrNull ?? '0') ?? 0;
+
+    return EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical);
+  }
+
+  static Map<String, MeiucaThemePropertyData> _createPropertyData(
+    EdgeInsets value,
+  ) {
+    final vertical = value.vertical;
+    final horizontal = value.vertical;
+
+    final propertyV = MeiucaThemePropertyData(
+      value: '${vertical}px',
+      attributes: MeiucaThemePropertyDataAttributes(
+        category: 'size',
+        type: 'spacing_squish',
+      ),
+    );
+
+    final propertyH = MeiucaThemePropertyData(
+      value: '${horizontal}px',
+      attributes: MeiucaThemePropertyDataAttributes(
+        category: 'size',
+        type: 'spacing_squish',
+      ),
+    );
+
+    final propertyStack = MeiucaThemePropertyData(
+      value: '${vertical}px ${horizontal}px',
+      attributes: MeiucaThemePropertyDataAttributes(
+        category: 'size',
+        type: 'spacing_squish',
+      ),
+    );
+
+    return {
+      _vertical: propertyV.toJSON(),
+      _horizontal: propertyH.toJSON(),
+      _stack: propertyStack.toJSON(),
+    };
+  }
+}
